0ca8a3bc35a01fddd0e7aeb63eea6f2c5ef0dd84
DavidAdedigba <67604458+DavidAdedigba@users.noreply.github.com>
2023-09-14 14:57:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,208 @@
+
+        //Make the color header stick onscroll
+
+      
+
+        // feature change based on color
+
+        function lavenderbtn() {
+            let colorEl = document.getElementById("color-el");
+            colorEl.textContent = "Lavender"
+            document.querySelector("#color-el").style.color = "#e6e6fa";
+            document.querySelector("#lavender").style.border = "3px solid #006bea";
+            document.querySelector("#beige").style.border = "0px";
+            document.querySelector("#beige").style.padding = "0px";
+            document.querySelector("#green").style.border = "0px";
+            document.querySelector("#green").style.padding = "0px";
+            document.querySelector("#phantom").style.border = "0px";
+            document.querySelector("#phantom").style.padding = "0px";
+            document.getElementById("phone_image").src = "images/s23/s23lavender/s23-main-lavender.png";
+            document.querySelector(".grid1").style.backgroundColor = "#000";
+            document.getElementById("grid1_text").style.color = "#e6e6fa";
+            document.getElementById("grid1_table").style.color = "#e6e6fa";
+            document.getElementById("grid1_image").style.filter = "none";
+            document.querySelector(" #grid4_text").style.color = "#e6e6fa";
+            document.getElementById("grid8_image").src = "images/galaxy/aiony-haust-3TLl_97HNJo-unsplash.jpg";
+            document.querySelector("#grid8_text").style.color = "#e6e6fa";
+            document.getElementById("grid8_arrow").style.color = "#e6e6fa";
+            document.getElementById("video1").style.filter = "none";
+            document.getElementById("video2").style.filter = "none";
+            document.getElementById("video3").style.filter = "none";
+            document.getElementById("grid10_image").src = "images/galaxy/810-gojo.png";
+            document.getElementById("grid10_text").style.color = "#e6e6fa";
+            document.getElementById("grid10_arrow").style.color = "#e6e6fa";
+            document.getElementById("grid11_image").src = "images/galaxy/selfie.jpg"
+            document.getElementById("grid11_text").style.color = "#e6e6fa";
+            document.getElementById("grid11_arrow").style.color ="#e6e6fa";
+
+        }
+
+        function beigebtn() {
+            let colorEl = document.getElementById("color-el");
+            colorEl.textContent = "Beige"
+            document.querySelector("#color-el").style.color = "#f5f5dc";
+            document.querySelector("#beige").style.border = "3px solid #006bea";
+            document.querySelector("#lavender").style.border = "0px ";
+            document.querySelector("#lavender").style.padding = "0px";
+            document.querySelector("#green").style.border = "0px";
+            document.querySelector("#green").style.padding = "0px";
+            document.querySelector("#phantom").style.border = "0px";
+            document.querySelector("#phantom").style.padding = "0px";
+            document.getElementById("phone_image").src="images/s23/s23beige/s23-main-beige.png";
+            document.querySelector(".grid1").style.backgroundColor = "#000";
+            document.getElementById("grid1_text").style.color = "#f5f5dc";
+            document.getElementById("grid1_table").style.color = "#f5f5dc";
+            document.getElementById("grid1_image").style.filter = "none";
+            document.querySelector(" #grid4_text").style.color = "#f5f5dc";
+            document.getElementById("grid8_image").src = "images/galaxy/pooja-chaudhary-bqnbKxiIGZI-unsplash.jpg";
+            document.querySelector("#grid8_text").style.color = "#f5f5dc";
+            document.getElementById("grid8_arrow").style.color = "#f5f5dc";
+            document.getElementById("video1").style.filter = "none";
+            document.getElementById("video2").style.filter = "none";
+            document.getElementById("video3").style.filter = "none";
+            document.getElementById("grid10_image").src = "images/galaxy/810-zenitsu.png";
+            document.getElementById("grid10_text").style.color = "#f5f5dc";
+            document.getElementById("grid10_arrow").style.color = "#f5f5dc";
+            document.getElementById("grid11_image").src = "images/galaxy/selfie-beige.jpg";
+            document.getElementById("grid11_text").style.color = "#f5f5dc";
+            document.getElementById("grid11_arrow").style.color = "#f5f5dc";
+        }
+
+        function greenbtn() {
+            let colorEl = document.getElementById("color-el");
+            colorEl.textContent = "Green" ;
+            document.querySelector("#color-el").style.color = "#6b706a";
+            document.querySelector("#green").style.border = "3px solid #006bea";
+            document.querySelector("#green").style.padding = "5px";
+            document.querySelector("#beige").style.border = "0px";
+            document.querySelector("#beige").style.padding = "0px";
+            document.querySelector("#lavender").style.border = "0px";
+            document.querySelector("#lavender").style.padding = "0px";
+            document.querySelector("#phantom").style.border = "0px";
+            document.querySelector("#phantom").style.padding = "0px";
+            document.getElementById("phone_image").src = "images/s23/s23green/s23-main-green.png";
+            document.querySelector(".grid1").style.backgroundColor = "#000";
+            document.getElementById("grid1_text").style.color = "#fff";
+            document.getElementById("grid1_table").style.color = "#fff";
+            document.getElementById("grid1_image").style.filter = "none";
+            document.querySelector(" #grid4_text").style.color = "#fff";
+            document.getElementById("grid8_image").src = "images/galaxy/nicole-geri-Eb1mX9ds7sc-unsplash.jpg";
+            document.querySelector("#grid8_text").style.color = "#fff";
+            document.getElementById("grid8_arrow").style.color = "#fff";
+            document.getElementById("video1").style.filter = "none";
+            document.getElementById("video2").style.filter = "none";
+            document.getElementById("video3").style.filter = "none";
+            document.getElementById("grid10_image").src = "images/galaxy/810-lover.png";
+            document.getElementById("grid10_text").style.color = "#6b706a";
+            document.getElementById("grid10_arrow").style.color = "#6b706a";
+            document.getElementById("grid11_image").src = "images/galaxy/selfie-green.jpg";
+            document.getElementById("grid11_text").style.color = "#fff";
+            document.getElementById("grid11_arrow").style.color = "#fff";
+        }
+
+        function phantombtn() {
+            let colorEl = document.getElementById("color-el");
+            colorEl.textContent = "Phantom" ;
+            document.querySelector("#color-el").style.color = "#000100";
+            document.querySelector("#phantom").style.border = "3px solid #006bea";
+            document.querySelector("#phantom").style.padding = "5px";
+            document.querySelector("#beige").style.border = "0px";
+            document.querySelector("#beige").style.padding = "0px";
+            document.querySelector("#green").style.border = "0px";
+            document.querySelector("#green").style.padding = "0px";
+            document.querySelector("#lavender").style.border = "0px";
+            document.querySelector("#lavender").style.padding = "0px";
+            document.getElementById("phone_image").src = "images/s23/s23black/s23-main-black.png";
+            document.querySelector(".grid1").style.backgroundColor = "#f2f2f2";
+            document.getElementById("grid1_text").style.color = "#000";
+            document.getElementById("grid1_text").style.color = "#000";
+            document.getElementById("grid1_table").style.color = "#000";
+            document.getElementById("grid1_image").style.filter = "grayscale(100%)";
+            document.querySelector(" #grid4_text").style.color = "#fff";
+            document.getElementById("grid8_image").src = "images/galaxy/kazuo-ota-KslV_udVgjI-unsplash.jpg";
+            document.querySelector("#grid8_text").style.color = "#fff";
+            document.getElementById("grid8_arrow").style.color = "#fff";
+            document.getElementById("video1").style.filter = "grayscale(100%)";
+            document.getElementById("video2").style.filter = "grayscale(100%)";
+            document.getElementById("video3").style.filter = "grayscale(100%)";
+            document.getElementById("grid10_image").src = "images/galaxy/810-luffy.png";
+            document.getElementById("grid10_text").style.color = "#fff";
+            document.getElementById("grid10_arrow").style.color = "#fff";
+            document.getElementById("grid11_image").src = "images/galaxy/selfie-black.jpg";
+            document.getElementById("grid11_text").style.color = "#fff";
+            document.getElementById("grid11_arrow").style.color = "#fff";
+        }
+
+
+       
+
+
+
+
+
+
+
+
+
+
+// play videos when visible
+
+var videos = document.getElementsByTagName("video");
+
+function checkScroll() {
+    var fraction = 0.8; // Play when 80% of the player is visible.
+
+    for(var i = 0; i < videos.length; i++) {
+
+        var video = videos[i];
+
+        var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+            b = y + h, //bottom
+            visibleX, visibleY, visible;
+
+            visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+            visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+            visible = visibleX * visibleY / (w * h);
+
+            if (visible > fraction) {
+                video.play();
+            } else {
+                video.pause();
+            }
+
+    }
+
+}
+
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
+
+
+$(function(){  // $(document).ready shorthand
+    $('#myNav').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('.grid__design').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+  });
